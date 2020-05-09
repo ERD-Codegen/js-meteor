888bdd3329fdfd1513671f3ef4bab27672b83fe5
Francois Lecroart <peernohell@gmail.com>
2020-05-09 18:38:14 +0200

    add eq helper

--- client/helpers.js
+++ client/helpers.js
@@ -1,3 +1,4 @@
+Template.registerHelper('eq', (a, b) => a === b);
 Template.registerHelper('instance', () => Template.instance());
 Template.registerHelper('isRouteName', (name) => FlowRouter.getRouteName() === name);
 Template.registerHelper('path', (pathDef, options) => FlowRouter.path(pathDef, options?.hash));
